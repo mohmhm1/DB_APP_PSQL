77bef89cdbc4655477649212e26ebd0822c66542
Ahmed Mahmoud <ahmedmahmoud86@ymail.com>
2016-03-01 15:53:40 -0500

    Updates for heroku deployment

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,23 @@
+class RegistrationsController < Devise::RegistrationsController
+  def new
+    flash[:info] = 'Registrations are not open.'
+    redirect_to about_path
+  end
+
+  def create
+    flash[:info] = 'Registrations are not open.'
+    redirect_to about_path
+  end
+  def update
+  
+
+  end
+
+
+
+
+end
+
+
+
+
