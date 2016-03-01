2f1f476644bf576ec4c1caf4b25b46821f735421
Ahmed Mahmoud <ahmedmahmoud86@ymail.com>
2016-03-01 15:59:50 -0500

    Updates for heroku deployment

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  
-  devise_for :users, :controllers => { :registrations => "registrations" 
-  
+  devise_for :users
   get 'sessions/new'
 
   get 'sessions/create'
